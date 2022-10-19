9df665a1f348becf596939560c04b77269ee6367
sandip <chavda@pradip>
2022-10-19 13:43:30 +0530

    Watched Haif The Video in This Commit

---
+++ first.js
@@ -0,0 +1,297 @@
+// var a;
+// a=22;
+//alert();
+// console.log( a);
+// document.getElementById("h").innerHTML="sandip";
+
+//document.write("chavda");
+
+/* var digit1=5;
+let digit2=7;
+
+digit1=10;
+digit2=10;
+
+console.log(digit1);
+console.log(digit2);
+console.log(digit1+digit2); */
+
+/* let x=5;
+let y=2;
+let z;
+ z=y ** 5;
+console.log(z) */
+
+let x=2;
+// let y=2;
+
+x++;
+// ++x;
+
+
+// let a='sandip';
+// let b='chavda';
+ 
+// console.log(x+y);
+// console.log(x);
+// console.log(y);
+
+// let age=18;
+	
+// if(age<=18){
+//     alert("you can not give vote!");
+// }else{
+//     alert("you can vote!");
+// }
+
+
+let input;
+	input=0;
+	
+// switch (input){
+//     case 1:
+//         document.wr("continue.....");
+//         break;
+//     case 0:
+//         document.write("End.....");
+//         break;    
+// } 
+	// switch(input){
+	// 	case 1:
+	// 		document.write("Continue......");
+	// 		break;
+	// 	case "y":
+	// 		document.write("Continue......");
+	// 		break;
+	// 	case "yes":
+	// 		document.write("Continue......");
+	// 		break;
+	// 	case 0:
+	// 		document.write("End......");
+	// 		break;
+	// 	case "n":
+	// 		document.write("End......");
+	// 		break;
+	// 	case "no":
+	// 		document.write("End......");
+	// 		break;
+	// 	default:
+	// 		document.write("wroge input");			
+	// }
+    // switch(input){
+	// 	case 1:
+	// 	case "y":
+	// 	case "yes":
+	// 		document.write("Continue......");
+	// 		break;
+	// 	case 0:
+	// 	case "n":
+	// 	case "no":
+	// 		document.write("End......");
+	// 		break;
+	// 	default:
+	// 		document.write("wroge input");			
+	// }
+
+    // let count=1;
+    // while (count<=10){
+    //     document.write("sandip,,");
+    //     count++;
+    // }
+
+    // let counter = 1;
+    // let sum = 0 ;
+    // while (counter <= 1000){
+    //     if(counter %2 == 0){
+    //         sum=sum+counter;
+    //     }
+    //     counter++;
+    // }
+    // document.write(sum);
+
+    // let counter = 1;
+			
+	// 		do{
+	// 			document.write("sandip,,");
+	// 			counter++;
+	// 		}while (counter <= 10);
+
+    // for(let counter=1; counter<=10; counter++){
+    //     document.write("sandip,");
+    // }
+
+    // for(let counter=1; counter<=10; counter++){
+    //     if(counter==5){
+    //             break;
+    //     }
+    //     document.write(counter);
+    //     document.write('<br>');
+    // }
+
+    // outer: for(let counter=1; counter<=10; counter++){  // <outer> name for a loop
+    //     if(counter==5){
+    //             continue outer;
+    //     }
+    //     document.write(counter);
+    //     document.write('<br>');
+    // }
+
+    // for(let counter=1; counter<=10; counter++){
+    //     document.write(counter);
+    //     document.write('<br>');
+    //     for(let counter2=1 ; counter2<3 ; counter2++){
+    //             document.write("sandip");
+    //             document.write('<br>');
+    //     }
+    // }
+
+    	// alert('welcome');
+
+
+        // let pelsn= prompt('enter your age');      
+        // if(age!=null){                           
+        //     console.log('hello');                <------------------------
+        // }else{
+        //     console.log('hiii'); 
+        // }
+        // (pelsn!=null)? document.write('not a null'): document.write('field is null');
+
+
+        // let responce = confirm('are you sure ,you want to delete ? ');
+        // if(responce){
+        //     document.write('deleted');
+        // }else{
+        //     document.write('not deleted'); 
+        // }
+
+        // let type = false;
+        // console.log(type);
+        // console.log(typeof type);
+
+        // let newType = String(type);
+        // console.log(newType);
+        // console.log(typeof newType);
+    
+        // let type = true;
+        // console.log(type);
+        // console.log(typeof type);
+        // let newType = Number(type);
+        // console.log(newType);
+        // console.log(typeof newType);
+
+        // let type = "false";
+		// 	console.log(type);
+		// 	console.log(typeof type);
+		// 	let newType = Boolean(type);
+		// 	console.log(newType);
+		// 	console.log(typeof newType);
+
+        // let uName = "sandip"
+        // let greet = `hii ${uName}`;
+        // console.log(greet);
+
+        // let str = "sandip \n \t \* chavda" ;
+        // console.log(str);
+
+        // let str ="sandip chavda";
+		// 	console.log(str.length);
+
+        // let str = "chavda sandip";
+		// console.log(str[3]) ;
+
+        // let str = "sandip chavda";
+        // if ("sandip chavda" == str){
+        //     console.log("equal");
+        // } 
+        // else {
+        //     console.log("not equal");
+        // }
+
+        // let str = "sandip";
+        // let str2 = "chavda";
+        // let str3 = str.concat(" ",str2);
+        // console.log(str3);
+
+        // let str = "This is javascript tutorial. It is a good tutorial. "
+        // let str2 = str.substring(8,26);
+        // console.log(str2);
+
+        // let str = "This is javascript tutorial. It is a good tutorial. "
+        // let position = str.indexOf("is",6);
+        // console.log(position);
+
+        // let str = "This is javascript tutorial. It is a good tutorial. "
+        // let position = str.startIndexOf("is");
+        // console.log(position);
+
+        // let str = "   This is javascript tutorial. It is a good tutorial.    ";
+        // let str2 = str.trimEnd();
+        // console.log(str);
+        // console.log(str2);
+
+        // let str = "  This is javascript tutorial. It is a good tutorial.   ";
+        // let str2 = str.toLowerCase();
+        // console.log(str);
+        // console.log(str2);	
+
+        // let str = "  This is javascript tutorial. It is a good tutorial.   ";
+		// 	let str2 = str.replace('good','best');
+		// 	console.log(str);
+		// 	console.log(str2);
+
+        // let str = "  This is javascript tutorial. It is a good tutorial.   ";
+        // let str2 = str.includes('javascript');
+        // console.log(str);
+        // console.log(str2);
+
+
+        // let book = ["math","physics","bio","computer"];
+		// 	let mark = [72,85,75,80,89];
+		// 	book[2] = "english"; // Update Array Values...
+		// 	console.log(book);
+		// 	console.log(mark[2]);
+
+        // let book = ["math","physics","bio","computer"];
+		// console.log(book.length);
+
+        // let book = ["math","physics","bio","computer"];
+        // book.push("hindi");
+        // console.log(book);
+
+        // let book = ["math","physics","bio","computer"];
+		// book.unshift("hindi");
+		// console.log(book);
+
+        // let book = ["math","physics","bio","computer"];		// End part Deleted 
+		// 		book.pop();
+		// 		console.log(book);
+
+        // let book = ["math","physics","bio","computer"];		// Start part Deleted 
+		// 		book.shift();
+		// 		console.log(book);	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
