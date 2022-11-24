9aa7c55893ec9a70c11a4ba378122e84806b86ae
sandip <chavda@pradip>
2022-11-24 20:15:42 +0530

    javascript events

--- first.js
+++ first.js
@@ -45,8 +45,8 @@ x++;
 // }
 
 
-let input;
-	input=0;
+// let input;
+// 	input=0;
 	
 // switch (input){
 //     case 1:
@@ -263,27 +263,300 @@ let input;
 		// console.log(book);
 
         // let book = ["math","physics","bio","computer"];		// End part Deleted 
-		// 		book.pop();
-		// 		console.log(book);
+		// 	book.pop();
+		// 	console.log(book);
 
         // let book = ["math","physics","bio","computer"];		// Start part Deleted 
-		// 		book.shift();
-		// 		console.log(book);	
+		// book.shift();
+		// console.log(book);	
+
+        // let book = ["math","physics","bio","computer"];		// Start part Deleted 
+		// book.splice(1,2); // 1 is a Stating Position and 2 is a Ending Position
+		// console.log(book);
+
+        // let book = ["math","physics","bio","computer"];				
+        // book.length = 0 ; // balnk Array 
+        // console.log(book);
+
+
+        // let book = ["math","physics","bio","computer"];				
+        // let position = book.indexOf("bio"); 
+        // console.log(position);
+
+        // let book = ["math","physics","bio","computer"];  //Array				
+        // let book = "math"; //variable
+        // console.log(Array.isArray(book));
+
+        
+        // let text =  "This is Random Text"			
+        // let wordarray = text.split(" ") ; // Array
+        // console.log(wordarray);
+
+
+        // let book = ["math","physics","bio","computer"];  //Array
+        // let wordarray = book.join(" ") ; // variable
+        // console.log(wordarray);
+
+
+        // let book = ["math","physics","bio","computer"];  
+		// let book2 = ["hindi","english"];  
+        // let book3 = ["telgu","punjabi"];  
+		// let newBook = book.concat(book2 , book3) ; 
+		// console.log(newBook); 
+
+        // let bookWithPages = [
+        //     ["math","300"],
+        //     ["physics","500"],
+        //     ["bio","200"]
+        // ];  
+        // console.log(bookWithPages[0][1]); // "300"
+        // console.log(bookWithPages[0][0]); // "math"
+        // console.log(bookWithPages[1][1]); // "500"
+        // console.log(bookWithPages[2][0]); // "bio"
+
+        // let book = ["math","physics","bio","computer"]; 
+        // let booklength = book.length;
+        // for(i=0;i<booklength ; i++){
+            //     console.log(`Element ${i} is ${book[i]} \n`);
+            // }
+            
+        // let book = ["math","physics","bio","computer"]; 
+        // book.forEach(function_name);
+        // function function_name(array_value){
+        //     console.log(array_value);
+        // }
+
+        // function MulTable(){
+		// 	for (i=1; i<=10; i++){
+		// 		document.write(`2 x ${i} = ${2*i}`);
+        //         document.write("<br>");
+		// 	}
+		// }
+        // MulTable();
+
+
+        // function MulTable(num){				// Parameter is num.
+		// 	for (i=1; i<=10; i++){
+		// 		document.write(`${num} x ${i} = ${num*i}`);
+		// 		document.write("<br>");
+		// 		}
+		// 	}
+
+            
+
+		// 	MulTable(1);			// Argument is 1.
+		// 	document.write("<br>");
+		// 	MulTable(2);			// Argument is 2.
+		// 	document.write("<br>");
+		// 	MulTable(3);			// Argument is 3.
+		// 	document.write("<br>");
+		// 	MulTable(4);			// Argument is 4.
+		// 	document.write("<br>");
+		// 	MulTable(5);			// Argument is 5.
+		// 	document.write("<br>");
+		// 	MulTable(6);			// Argument is 6.
+		// 	document.write("<br>");
+		// 	MulTable(7);			// Argument is 7.
+		// 	document.write("<br>");
+		// 	MulTable(8);			// Argument is 8.
+		// 	document.write("<br>");
+		// 	MulTable(9);			// Argument is 9.
+		// 	document.write("<br>");
+		// 	MulTable(10);			// Argument is 10.
+
+
+            // function add(num1,num2){				// Parameter is num1 , num2 .
+                
+            //         document.write(`${num1} + ${num2} = ${num1+num2}<br>`);
+            //         document.write(`${num1} - ${num2} = ${num1-num2}<br>`);
+            //         document.write(`${num1} x ${num2} = ${num1*num2}<br>`);
+            //         document.write(`${num1} / ${num2} = ${num1/num2}<br>`);
+            //         document.write('<br>');
+                    
+            //     }
+            //  add(12,15); 			// Argument is 12 and 15.
+            //  add(1000,905); 			// Argument is 1000 and 905.
+            //  add(12051,13157); 			// Argument is 12051 and 13157.
+
+            // function add(){
+			// 	console.log(arguments[1]);
+			// }
+			// add(1,7,9);  // output is 7
+
+            // function add(){
+
+            //     if(arguments.length == 0){
+            //         console.log("No Parameter Passed !");
+            //     } else{
+            //         let sum=0;
+            //         let min=0;
+            //         for(let i = 0; i<arguments.length; i ++){
+            //             sum=sum+arguments[i];
+            //         }
+            //         for(let i = 0; i<arguments.length; i ++){
+            //             min=min-arguments[i];
+            //         }
+
+            //         console.log(sum); 
+            //         console.log(min);
+            //     }
+            // }
+            // add(10,10,10);
+
+
+            
+            // let addition=add; 
+
+
+            // function add(a , b){
+            //     return a + b ;
+            //     // console.log(a + b);  // Not Wrorking
+            //     // document.write(a + b); //Not Wrorking
+            // }
+            // let store=add(5,6);
+            // document.write(store);
+
+            // let car='audi';     // Global Variable
+            // function add(){
+            //     let result = 33 ;   // Local Variable
+            //     console.log(result);
+            //     console.log(car);
+            // }
+            // add();
+            // console.log(car);
+
+            // function sum(){
+            //     let result = 33 ;   // Local Variable
+            //     console.log(result);
+            // }
+            // function add(){
+            //     let result = 53 ;   // Local Variable
+            //     console.log(result);
+            // }
+            // sum();
+            // add();
+
+            // let show = function (){
+            //     console.log('hello');
+            // };
+            // show();
+
+            // setTimeout(function(){
+            //     console.log('hello');
+            // },2000);
+
+            // (function (){		
+            //     console.log('hello');
+            // })();
+
+            // let person = {
+			// 	firstName : 'sandip',
+			// 	lastName : 'chavda',
+            //     sayHello : function(){
+            //         console.log("hello4 !");
+            //     }
+            //     sayHello  (){
+            //         console.log("hello3 !");
+            //     }
+            // }
+            // person.firstName = 'sandipbhai';
+            // person.age = 22;
+
+            // console.log(person.agea);
+            // console.log(person.firstName);
+            // console.log(person.lastName);
+            // console.log(person['firstName']);
+            // console.log(person['lastName']);
+            // console.log('firstName' in person);
+            // for (let key in person){
+			// 	console.log (key +": "+ person.key);
+			// }
+
+            // person.sayHello = function (){
+            //     console.log("hello !");
+            // }
+            
+            // function greet(){
+            //     console.log("hello2 !");
+            // }
+            // person.sayHello = greet;
+            // person.sayHello();
+
+
+            // let person ={
+            //     firstName : "sandip",
+            //     lastName : "chavda" ,
+            //     sayHello (){
+            //         console.log("hello ! I am " + person.firstName + " and i have a " +car.brand + " car"); 
+            //         console.log("hello ! I am " + this.firstName + " and i have a " +car.brand + " car"); 
+            //     }
+            // };
+            // let car ={
+            //     brand : "tata",
+            //     model : "safari"
+            // }
+            // person.sayHello();
+            // let num=Math.PI;
+            // let num=Math.E;
+            // console.log(num);
+            // var xa = Math.round(4.1);
+	        // console.log(xa);
+            // var xe = Math.ceil(5.8);
+            // console.log(xe);
+            // var xe = Math.floor(5.8);
+            // console.log(xe);
+            // var xe = Math.trunc(5.8);
+            // console.log(xe);
+            // var num = Math.pow(2 , 3);
+	        // console.log(num);
+            // var num = Math.sqrt(100);
+	        // console.log(num);	
+            // var num1 = Math.max(10,285,102,45,25,68);
+	        // console.log(num1);		
+            // var num2 = Math.min(10,285,102,45,25,68);
+	        // console.log(num2);		
 
 
 
+            // let randomNumber = Math.random();  // 0 to 1.
+            // let randomNumber = Math.random() * 10;  // 0 to 9.
+            // let randomNumber = Math.floor( Math.random() * 10);  // 0 to 9.
+            // let randomNumber = Math.floor( Math.random() * 11);  // 0 to 10.
+            // let randomNumber =  Math.floor( Math.random() * 101);  // 0 to 99.
+            // let randomNumber = Math.floor( Math.random() * 10) + 1;  // 1 to 10
+            // let randomNumber = Math.floor( Math.random() * (25 - 15)) + 15;  // 15 to 24
+            // function getRandom (min , max){
+            //     let randomNumber = Math.floor( Math.random() * (max - min)) + min; 
+            //     return randomNumber;
+            // }
+            
+            // console.log(getRandom(10,20));
 
-
-
-
-
-
-
-
-
-
-
-
+            // let date =  Date ();  // store current date. 
+            // console.log(date);
+            // let date = new Date (2006,11,12);  
+            // let date = new Date ("october 13,2018 11:12:33");  
+            // // date.setDate(25);
+            // // date.setMonth(5);
+            // // date.setFullYear(2022);
+            // // date.setHours(10);
+            // // date.setMinutes(25);
+            // // date.setSeconds(08);
+            // // console.log(date); 
+            // // console.log(date.getDay()); 
+            // date.setDate( date.getDate() + 50);
+            // console.log(date); 
+            // let date1 = new Date ("october 13,2018 11:12:33");  
+			// let date2 = new Date (2015,04,16,12,45,33);  
+			
+			// if (date1<date2){
+			// 	console.log("Entered date is Past date")
+			// }else if(date1>date2){
+			// 	console.log("Entered date is future date")
+			// }else{
+			// 	console.log("Entered date Same Date")
+			// }
+		
 
 
 
